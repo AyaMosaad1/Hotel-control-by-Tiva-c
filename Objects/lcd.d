ab4cedd795519de3687629b7595b935b594ad82c
AyaMosaad1 <Ayamosad37@gmail.com>
2020-06-01 16:20:02 +0200

    init project

---
+++ Objects/lcd.d
@@ -0,0 +1,6 @@
+.\objects\lcd.o: lcd.c
+.\objects\lcd.o: lcd.h
+.\objects\lcd.o: tm4c123gh6pm.h
+.\objects\lcd.o: common_macros.h
+.\objects\lcd.o: delay.h
+.\objects\lcd.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
